19f96bf1ba07d98cbf775f32a8f2f242e117b34e
Aditya Suri <46070970+SuriAditya@users.noreply.github.com>
2020-03-20 00:48:12 +0530

    Update copy.sh

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,4 +1,4 @@
 if [ "$DEPLOYMENT_GROUP_NAME" == "DemoTestDeploymentGroup" ]
 then
-    pscp -r /var/www/html/demo/ /var/www/html/development
+    mv /var/www/html/demo/ /var/www/html/development
 fi
