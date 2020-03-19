703972e547d529e6f7ec5269fb1a72aecbfa2a7e
Aditya Suri <46070970+SuriAditya@users.noreply.github.com>
2020-03-20 01:14:09 +0530

    -r

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,4 +1,4 @@
 if [ "$DEPLOYMENT_GROUP_NAME" == "DemoTestDeploymentGroup" ]
 then
-    mv -f /var/www/html/container/ /var/www/html/development
+    mv -f -r /var/www/html/container/ /var/www/html/development
 fi
