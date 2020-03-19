72b2fdb692908595220f75416f57e23c917f1670
Aditya Suri <46070970+SuriAditya@users.noreply.github.com>
2020-03-20 00:38:22 +0530

    Update copy.sh

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,4 +1,4 @@
 if [ "$DEPLOYMENT_GROUP_NAME" == "DemoTestDeploymentGroups" ]
 then
-    scp -i /var/www/html/demo -r /var/www/html/development
+    cp -r /var/www/html/demo /var/www/html/development
 fi
