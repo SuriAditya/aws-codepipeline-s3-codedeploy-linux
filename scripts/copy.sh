f7f6d99c925664ee16da0071c3d32ff7152d55fe
Aditya Suri <46070970+SuriAditya@users.noreply.github.com>
2020-03-20 02:46:48 +0530

    Update with production

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,7 +1,6 @@
 if [ "$DEPLOYMENT_GROUP_NAME" == "DemoTestDeploymentGroup" ]
 then
     sudo rsync -av /var/www/html/container/* /var/www/html/development --delete
-fi
 elif [ "$DEPLOYMENT_GROUP_NAME" == "DeploymentGroup" ]
 then
     sudo rsync -av /var/www/html/container/* /var/www/html/production --delete
