dc3433264a608948533f12e10e4eda4cb6d61e70
Aditya Suri <46070970+SuriAditya@users.noreply.github.com>
2020-03-20 02:16:10 +0530

    Update copy.sh

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,4 +1,4 @@
 if [ "$DEPLOYMENT_GROUP_NAME" == "DemoTestDeploymentGroup" ]
 then
-    sudo rsync -av /var/www/html /var/www/html/development
+    sudo rsync -av /var/www/html/container /var/www/html/development
 fi
